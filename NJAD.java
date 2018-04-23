9ad528701f2831e7874136a06afd2059528e2b7e
Grace Xin <gracexin2003@users.noreply.github.com>
2018-04-22 18:49:38 -0700

    several test cases

--- NJAD.java
+++ NJAD.java
@@ -15,7 +15,15 @@ class NJAD {
 		s.nextLine();
 		for(int i = 0; i < N; i++) grid[i] = s.nextLine().toCharArray();
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {}
+			int count1 = 0, count2 = 0;
+			for(int j = 0; j < N; j++) {
+				if(grid[i][j] == 'F') count1++;
+				if(grid[j][i] == 'F') count2++;
+			}
+			if(count1 == 1 || count2 == 1 || count1 == N || count2 == N) {
+				System.out.println("RI");
+				return;
+			}
 		}
 		
 	}
