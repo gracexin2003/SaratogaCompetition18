bdb03daf6deae02d57fbc0ca6239dc82126b233e
Grace Xin <gracexin2003@users.noreply.github.com>
2018-04-22 18:40:52 -0700

    E and J inputs

--- NHAE.java
+++ NHAE.java
@@ -5,9 +5,26 @@ import java.util.*;
 @SuppressWarnings("resource")
 class NHAE {
 
+	static int N;
+	static class Object {
+		int weight;
+		ArrayList<Object> adjacent = new ArrayList<Object>();
+		public Object(int w) {weight = w;}
+	}
+	static Object objects[];
+	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
+		N = s.nextInt(); objects = new Object[N];
+		for(int i = 0; i < N; i++) objects[i] = new Object(s.nextInt());
+		for(int i = 0; i < N-1; i++) {
+			int a = s.nextInt(), b = s.nextInt();
+			objects[a-1].adjacent.add(objects[b-1]);
+			objects[b-1].adjacent.add(objects[a-1]);
+		}
+		//the hell?
+		
 		
 	}
 	
--- NJAD.java
+++ NJAD.java
@@ -5,9 +5,18 @@ import java.util.*;
 @SuppressWarnings("resource")
 class NJAD {
 
+	static int N;
+	static char[][] grid;
+	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
+		N = s.nextInt(); grid = new char[N][N];
+		s.nextLine();
+		for(int i = 0; i < N; i++) grid[i] = s.nextLine().toCharArray();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {}
+		}
 		
 	}
 	
