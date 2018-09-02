45ab2f5751e7db0805941d978c76bee84ef4fa01
Grace Xin <gracexin2003@users.noreply.github.com>
2018-09-02 16:19:40 -0700

    just changes...

--- NLAG_Grace.java
+++ NLAG_Grace.java
@@ -2,14 +2,18 @@ package SaratogaCompetition18;
 
 import java.util.*;
 
-@SuppressWarnings({"resource", "unused"})
+@SuppressWarnings("resource")
 class NLAG_Grace {
 
+	public static int N, L1, L2;
+	public static String[] strings;
+	public static String greatest;
+	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		int N = s.nextInt(), L1 = s.nextInt(), L2 = s.nextInt();
-		for(int i = 0; i < 3; i++) {}
+		N = s.nextInt(); L1 = s.nextInt(); L2 = s.nextInt();
+		for(int i = 0; i < 3; i++) strings[i] = s.nextLine();
 		
 		
 	}
