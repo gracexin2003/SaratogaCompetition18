f23f6c4705b5de6ae5a74d208fe70b3c1c24c6b1
Grace Xin <gracexin2003@users.noreply.github.com>
2018-09-02 21:10:38 -0700

    adjacency list
    changing the space-consuming adjacency matrix into an adjacency list

--- NHAE_Grace.java
+++ NHAE_Grace.java
@@ -2,57 +2,69 @@ package SaratogaCompetition18;
 
 import java.util.*;
 
-@SuppressWarnings("resource")
+@SuppressWarnings ("resource")
 class NHAE_Grace {
 	
 	// dfs + dp for weights during backtrack
 	
 	public static int N;
-	public static int[][] adjMat;
+	public static List[] adjList;
 	public static int[] weights;
 	public static boolean[] visited;
 	public static int totalW;
 	
-	public static void main(String[] args) {
+	public static class List { ArrayList<Object> l = new ArrayList<Object>(); }
+	public static class Object { 
+		int weight, node; 
+		public Object(int w, int n) {
+			weight = w; node = n;
+		}
+		public String toString() {
+			return weight + "/" + node;
+		}
+	}
+	
+	public static void main (String[] args) {
 		
 		Scanner s = new Scanner(System.in);
 		N = s.nextInt(); 
 		weights = new int[N];
-		adjMat = new int[N][N];
+		adjList = new List[N];
 		visited = new boolean[N];
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			weights[i] = s.nextInt();
 			totalW += weights[i];
 		}
-		for(int i = 0; i < N-1; i++) {
+		for (int i = 0; i < N; i++) adjList[i] = new List();
+		for (int i = 0; i < N-1; i++) {
 			int a = s.nextInt()-1, b = s.nextInt()-1;
-			adjMat[a][b] = 1;
-			adjMat[b][a] = 1;
+			adjList[a].l.add(new Object(0, b));
+			adjList[b].l.add(new Object(0, a));
 		}
+		
+		for (int i = 0; i < N; i++)
+			System.out.println(adjList[i].l.toString());
+		System.out.println();
+		
 		dfsWeights(0);
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) 
-				System.out.print(adjMat[i][j] + " ");
-			System.out.println();
-		}
+		for (int i = 0; i < N; i++)
+			System.out.println(adjList[i].l);
 		
 		int ans = 0;
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			int onew = 0;
 			boolean b = false;
-			for(int j = 0; j < N; j++) {
-				if(adjMat[i][j] != 0) {
-					if (onew > 0 && adjMat[i][j] != onew) {
-						System.out.println("BREAK");
-						b = true;
-						break;
-					}
-					onew = adjMat[i][j];
-					System.out.println(i + "," + j + " - " + onew);
+			for (Object o : adjList[i].l) {
+				if(onew > 0 && o.weight != onew) {
+					System.out.println("BREAK");
+					b = true;
+					break;
 				}
+				onew = o.weight;
+				System.out.println(o.node + " - " + onew);
 			}
-			if(b) continue;
+			if (b) continue;
 			ans++;
 			System.out.println("ans = " + ans);
 		}
@@ -60,21 +72,28 @@ class NHAE_Grace {
 		
 	}
 	
-	public static int dfsWeights(int i) {
-		if(visited[i]) return 0;
+	public static int dfsWeights (int i) {
+		if (visited[i]) return 0;
 		visited[i] = true;
 		int weight = weights[i];
 		int ws = 0;
-		for(int x = 0; x < N; x++) {
-			if(adjMat[i][x] != 0) {
-				int w = dfsWeights(x);
-				ws += w;
-				adjMat[i][x] = w;
-				adjMat[x][i] = totalW - w;
-			}
+		for (Object o : adjList[i].l) {
+			int w = dfsWeights(o.node);
+			ws += w;
+			o.weight = w;
+			adjList[o.node].l.get(index(o.node, i)).weight = totalW - w;
 		}
 		visited[i] = false;
 		return weight+ws;
+	}
+
+	public static int index(int listIndex, int wantedIndex) {
+		int i = 0;
+		for (Object o : adjList[listIndex].l) {
+			if (o.node == wantedIndex) return i;
+			i++;
+		}
+		return 0;
 	}
 	
 }
