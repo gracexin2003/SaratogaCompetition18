6549d6b72326d6cfbd04d84468be04b4c32b7f10
Grace Xin <gracexin2003@users.noreply.github.com>
2018-09-02 22:22:55 -0700

    dunno

--- NLAG_Grace.java
+++ NLAG_Grace.java
@@ -2,18 +2,18 @@ package SaratogaCompetition18;
 
 import java.util.*;
 
-@SuppressWarnings("resource")
+@SuppressWarnings ("resource")
 class NLAG_Grace {
 
 	public static int N, L1, L2;
 	public static String[] strings;
 	public static String greatest;
 	
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		
 		Scanner s = new Scanner(System.in);
 		N = s.nextInt(); L1 = s.nextInt(); L2 = s.nextInt();
-		for(int i = 0; i < 3; i++) strings[i] = s.nextLine();
+		for (int i = 0; i < 3; i++) strings[i] = s.nextLine();
 		
 		
 	}
